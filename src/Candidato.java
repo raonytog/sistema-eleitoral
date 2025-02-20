5e138ff31e668bcc87e41f98e1233dee3a7d66e6
raonytog <togneriraony@gmail.com>
2025-02-20 00:27:29 -0300

    inicio

---
+++ src/Candidato.java
@@ -0,0 +1,14 @@
+
+import java.util.Date;
+
+public class Candidato {
+    private String  nomeCandidato;
+    private int     numCandidato;
+
+    private int     votosTotais;
+
+    private Date    data;
+
+    private boolean codCandidatoEleito;
+    private boolean genero;
+}
---
+++ src/Partido.java
@@ -0,0 +1,7 @@
+public class Partido {
+    private int     numPartido;
+    private String  siglaPArtido;
+    private int     numFederacao;
+
+    private int     totalVotos;
+}
